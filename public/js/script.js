904bc7774ef217ddcff486883cb224a05e80cfc6
pikiavila <wowaura833@gmail.com>
2024-05-30 00:42:26 -0300

    arreglando api

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,28 @@
+// API
+
+const container = document.getElementById('imageContainer')
+
+fetch('https://rickandmortyapi.com/api/character')
+    .then(response => response.json())
+    .then(datos => {
+        datos.results.forEach(element => {
+
+            const div = document.createElement('div');
+            const img = document.createElement('img')
+            img.src = element.image;
+            img.alt = element.name;
+            img.style.width = '200px';
+            const p = document.createElement('p');
+            p.textContent = element.name;
+            div.appendChild(img);
+            div.appendChild(p);
+            container.appendChild(div);
+        })
+    })
+    .catch(err => console.log(err))
+
+// REGISTRO
+
 document.getElementById('formularioRegistro').addEventListener('submit', function (event) {
     event.preventDefault();
 
@@ -42,3 +67,5 @@ function validar(formularioRegistro) {
     }
 
 }
+
+
