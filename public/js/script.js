35d01481b674b2171981c395d17a6078b0b48a2e
Mili <moonstone3798@gmail.com>
2024-05-30 23:03:03 -0300

    validación del login agregada

--- public/js/script.js
+++ public/js/script.js
@@ -67,38 +67,3 @@ function validar(formularioRegistro) {
     }
 
 }
-document.getElementById('formularioLogin').addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    if (validarLogin(this)) {
-        this.submit();
-    }
-})
-
-//LOGIN
-function validarLogin(formularioLogin) {
-    let usuarioReg = formularioLogin.correoRegistrado.value;
-    let claveReg = formularioLogin.claveRegistrada.value;
-
-
-    if (usuarioReg != 'juan@hotmail.com' && claveReg != '1234') {
-        alert('Datos incorrectos');
-        return false;
-
-    } else {
-
-        window.location = "index.html";
-    }
-    expReg = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
-
-    if (!expReg.test(usuarioReg)) {
-        alert('Debe ingresar un correo válido');
-        return false;
-
-    }
-    if (claveReg.trim().length == 0) {
-        alert('La contraseña es requerida');
-        return false;
-    }
-
-}
