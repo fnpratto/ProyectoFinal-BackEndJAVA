a17abf43a9f586dc6cc009f6d37e60335f99fd1e
Mili <moonstone3798@gmail.com>
2024-05-31 10:51:18 -0300

    agregue redirección cuando se registran correctamente

--- public/js/script.js
+++ public/js/script.js
@@ -31,39 +31,53 @@ document.getElementById('formularioRegistro').addEventListener('submit', functio
     }
 })
 function validar(formularioRegistro) {
-    if (formularioRegistro.nombre.value.length == 0) {
-        alert('El campo nombre es requerido');
-        return false;
-    }
-
-    if (formularioRegistro.apellido.value.length == 0) {
-        alert('El campo apellido es requerido');
-        return false;
-    }
     expReg = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+    let nombreValido = formularioRegistro.nombre.value.length != 0;
+    let apellidoValido = formularioRegistro.apellido.value.length != 0;
+    let correoValido = expReg.test(formularioRegistro.correo.value);
+    let claveValida = formularioRegistro.clave.value.trim().length != 0;
+    let paisValido = formularioRegistro.pais.value != "";
+    let fechaValida = formularioRegistro.fecha.value.length != 0;
+    let terminosValidos = formularioRegistro.terminos.checked;
 
-    if (!expReg.test(formularioRegistro.correo.value)) {
-        alert('Debe ingresar un correo válido');
-        return false;
+    if (nombreValido && apellidoValido && correoValido && claveValida && paisValido && fechaValida && terminosValidos) {
+        window.location = "../index.html";
+    } else {
+        if (!nombreValido) {
+            alert('El campo nombre es requerido');
+            return false;
+        }
 
+        if (!apellidoValido) {
+            alert('El campo apellido es requerido');
+            return false;
+        }
+
+        if (!correoValido) {
+            alert('Debe ingresar un correo válido');
+            return false;
+
+        }
+        if (!claveValida) {
+            alert('La contraseña es requerida');
+            return false;
+        }
+
+        if (!paisValido) {
+            alert('El campo país es requerido');
+            return false;
+        }
+
+        if (!fechaValida) {
+            alert('El campo fecha es requerido');
+            return false;
+        }
+
+        if (!terminosValidos) {
+            alert('Debe aceptar los terminos y condiciones');
+            return false;
+        }
     }
 
-    if (formularioRegistro.clave.value.trim().length == 0) {
-        alert('La contraseña es requerida');
-        return false;
-    }
-    if (formularioRegistro.pais.value == "") {
-        alert('El campo país es requerido');
-        return false;
-    }
-    if (formularioRegistro.fecha.value.length == 0) {
-        alert('El campo fecha es requerido');
-        return false;
-    }
-
-    if (!formularioRegistro.terminos.checked) {
-        alert('Debe aceptar los terminos y condiciones');
-        return false;
-    }
 
 }
