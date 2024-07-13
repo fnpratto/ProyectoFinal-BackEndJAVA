c243e952573b214bd074abf474b5254f6ad270a0
Mili <moonstone3798@gmail.com>
2024-07-12 21:42:54 -0300

    algunos cambios, falta arreglar el create

--- public/js/scriptlogin.js
+++ public/js/scriptlogin.js
@@ -16,7 +16,7 @@ function validarLogin(formularioLogin) {
         alert('Datos incorrectos');
         return false;
     } else {
-        window.location = "../index.html";
+        window.location = "../views/admin.html";
     }
     expReg = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
 
